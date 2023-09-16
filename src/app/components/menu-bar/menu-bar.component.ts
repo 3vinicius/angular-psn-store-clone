7c0d1097a31592a7a6024022d793c487b934e6c6
3vinicius <amorimvinicius3@gmail.com>
2023-09-16 20:21:52 -0300

    style: add correct links

--- src/app/components/menu-bar/menu-bar.component.ts
+++ src/app/components/menu-bar/menu-bar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Output,EventEmitter } from '@angular/core';
 
+
 @Component({
   selector: 'app-menu-bar',
   templateUrl: './menu-bar.component.html',
@@ -10,6 +11,7 @@ export class MenuBarComponent implements OnInit {
   constructor() { }
 
 	@Output() nameForSearch = new EventEmitter();
+
 
   ngOnInit(): void {
   }
